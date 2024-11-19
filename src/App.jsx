ff582c8f1611fdd057953d4af8f275e04fb121cf
Nikhil Rawat <nikhilrawaa311@gmail.com>
2024-11-19 22:26:47 +0530

    Created Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Login from './components/Auth/Login'
 import EmpDashboard from './components/Dashboard/EmpDashboard'
 import Header from './components/Other/Header'
 import TaskNumbers from './components/Other/TaskNumbers'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 
 const App = () => {
   return (
     <>
       {/* <Login/> */}
-      <EmpDashboard / > 
-    
+      {/* <EmpDashboard / >  */}
+      <AdminDashboard/>
     </>
   )
 }
